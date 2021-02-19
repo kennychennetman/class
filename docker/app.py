517a195c8260a4ea8ebad00f06ddb9c74f47cad6
Kenny Chen <kenny@brobridge.com>
2021-02-19 11:42:39 +0800

    for cht class

---
+++ docker/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import os
+import socket
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    html = "<h3>Hello {name}!</h3>" \
+           "<font color='{color}'>Version: {version}</font><br/>"\
+           "<b>Hostname:</b> {hostname}<br/>"
+    return html.format(name=os.getenv("NAME", "world"), color=os.getenv("COLOR", "red"), version=os.getenv("VERSION", "v0"), hostname=socket.gethostname())
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
+
